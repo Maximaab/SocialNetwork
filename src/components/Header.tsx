de3d232ada89fd1c144c4e52d5d0b6b4f3f24296
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-07 21:02:10 +0300

    Commit1

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className="header"><img
+            src="https://s.tmimgcdn.com/scr/800x500/314400/n-initial-business-name-logo-vector-design-v6_314408-original.jpg"/>
+        </header>
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <footer className="content">
+            <div>
+                <img
+                    src="https://s3.eu-central-1.amazonaws.com/aviata-blog-2.0/blog/posts/optimized/0_a10d989e346bb54487284cbe0d52d061449320a4.png.webp"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </footer>
+    );
+};
+
