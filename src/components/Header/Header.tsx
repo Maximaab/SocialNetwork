f93b27a6446630edc7ab26e6c297dd9ec849f9ae
Maximaab <141754764+Maximaab@users.noreply.github.com>
2023-10-21 17:39:57 +0300

    first commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from "./Header.module.css";
 export const Header = () => {
     return (
-        <header className="header"><img
+        <header className={s.header}><img
             src="https://s.tmimgcdn.com/scr/800x500/314400/n-initial-business-name-logo-vector-design-v6_314408-original.jpg"/>
         </header>
     );
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from "./Profile.module.css"
 export const Profile = () => {
     return (
-        <footer className="content">
+        <footer className={s.content}>
             <div>
                 <img
                     src="https://s3.eu-central-1.amazonaws.com/aviata-blog-2.0/blog/posts/optimized/0_a10d989e346bb54487284cbe0d52d061449320a4.png.webp"/>
@@ -16,11 +16,11 @@ export const Profile = () => {
                     New post
                 </div>
             </div>
-            <div>
-                <div>
+            <div className="posts">
+                <div className={s.item}>
                     post 1
                 </div>
-                <div>
+                <div className={s.item}>
                     post 2
                 </div>
             </div>
